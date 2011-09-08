65abdde5e44c6e52b8ed33a324a46190bfbf84d5
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2011-09-08 23:11:50 +0200

    Merge camplight.py and camplight

--- camplight.py
+++ camplight.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python
+
 """
-Campfire API implementation in Python
+Campfire command-line client
+
+Usage: camplight.py <command> [<args>]
 
 The API is described at http://developer.37signals.com/campfire/index
 """
@@ -18,6 +22,7 @@ def json_encode(obj=None):
 
 def json_decode(s):
     return json.loads(s) if s.startswith('{') else {}
+
 
 class Campfire(object):
     def __init__(self, url, token):
@@ -136,3 +141,60 @@ class Sound(object):
     rimshot = 'rimshot'
     trombone = 'trombone'
     yeah = 'yeah'
+
+
+if __name__ == '__main__':
+    import sys
+    import os
+
+    # TODO add proper option handling
+    def handle_cmd(campfire, args):
+        cmd = args[1]
+        if cmd == 'rooms':
+            return campfire.rooms()
+        elif cmd == 'user':
+            return campfire.user(args[2])
+        elif cmd == 'presence':
+            return campfire.presence()
+        elif cmd == 'search':
+            return campfire.search(args[2])
+        else:
+            room_id = os.environ['CAMPFIRE_ROOM']
+            room = campfire.room(room_id)
+            if cmd == 'show':
+                return room.show()
+            elif cmd == 'name':
+                return room.set_name(args[2])
+            elif cmd == 'topic':
+                return room.set_topic(args[2])
+            elif cmd == 'recent':
+                return room.recent()
+            elif cmd == 'transcript':
+                return room.transcript()
+            elif cmd == 'uploads':
+                return room.uploads()
+            elif cmd == 'join':
+                return room.join()
+            elif cmd == 'leave':
+                return room.leave()
+            elif cmd == 'lock':
+                return room.lock()
+            elif cmd == 'unlock':
+                return room.unlock()
+            elif cmd == 'speak':
+                return room.speak(args[2])
+            elif cmd == 'paste':
+                return room.paste(args[2])
+            elif cmd == 'play':
+                return room.play(args[2])
+            else:
+                raise Exception('invalid command')
+
+    token = os.environ['CAMPFIRE_TOKEN']
+    url = os.environ['CAMPFIRE_URL']
+    campfire = Campfire(url, token)
+    data = handle_cmd(campfire, sys.argv)
+    if data:
+        # HACK re-encode json for pretty output
+        import simplejson as json
+        print json.dumps(data, indent=4)

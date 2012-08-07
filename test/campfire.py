493635d17d147c104085311960d479f102749295
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-08-07 20:37:14 +0200

    Use unittest's assertEqual()

--- test/campfire.py
+++ test/campfire.py
@@ -16,35 +16,35 @@ class CampfireTest(unittest.TestCase):
         self.request.get.return_value = {'account': expect}
         result = self.campfire.account()
         self.request.get.assert_called_once_with('/account')
-        assert result == expect
+        self.assertEqual(result, expect)
 
     def test_rooms(self):
         expect = [{'name': 'Serious'}, {'name': 'Danger'}]
         self.request.get.return_value = {'rooms': expect}
         result = self.campfire.rooms()
         self.request.get.assert_called_once_with('/rooms')
-        assert result == expect
+        self.assertEqual(result, expect)
 
     def test_user(self):
         expect = {'name': 'John Doe', 'email_address': 'john.doe@gmail.com'}
         self.request.get.return_value = {'user': expect}
         result = self.campfire.user()
         self.request.get.assert_called_once_with('/users/me')
-        assert result == expect
+        self.assertEqual(result, expect)
 
     def test_presence(self):
         expect = [{'name': 'Danger'}]
         self.request.get.return_value = {'rooms': expect}
         result = self.campfire.presence()
         self.request.get.assert_called_once_with('/presence')
-        assert result == expect
+        self.assertEqual(result, expect)
 
     def test_search(self):
         expect = [{'body': 'ohai', 'type': camplight.MessageType.TEXT}]
         self.request.get.return_value = {'messages': expect}
         result = self.campfire.search('ohai')
         self.request.get.assert_called_once_with('/search/ohai')
-        assert result == expect
+        self.assertEqual(result, expect)
 
 
 if __name__ == '__main__':

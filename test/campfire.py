cc1e865d7adf8f968b2695ec064da46dd82c88e8
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-08-08 15:49:26 +0200

    Add room and user tests

--- test/campfire.py
+++ test/campfire.py
@@ -25,11 +25,34 @@ class CampfireTest(unittest.TestCase):
         self.request.get.assert_called_once_with('/rooms')
         self.assertEqual(result, expect)
 
-    def test_user(self):
+    def test_room_by_name(self):
+        rooms = [{'name': 'Serious', 'id': 1000}, {'name': 'Danger', 'id': 2000}]
+        self.campfire.rooms = mock.Mock(return_value=rooms)
+        result = self.campfire.room('Serious')
+        self.assertEqual(result.room_id, 1000)
+
+    def test_room_by_id(self):
+        result = self.campfire.room(3000)
+        self.assertEqual(result.room_id, 3000)
+
+    def test_room_not_found(self):
+        rooms = [{'name': 'Serious', 'id': 1000}]
+        self.campfire.rooms = mock.Mock(return_value=rooms)
+        self.assertRaises(camplight.RoomNotFoundError, self.campfire.room, 'Danger')
+
+    def test_user_me(self):
         expect = {'name': 'John Doe', 'email_address': 'john.doe@gmail.com'}
         self.request.get.return_value = {'user': expect}
         result = self.campfire.user()
         self.request.get.assert_called_once_with('/users/me')
+        self.assertEqual(result, expect)
+
+    def test_user_other(self):
+        expect = {'name': 'Alan Turing'}
+        self.request.get.return_value = {'user': expect}
+        user_id = 6789
+        result = self.campfire.user(user_id)
+        self.request.get.assert_called_once_with('/users/%s' % user_id)
         self.assertEqual(result, expect)
 
     def test_presence(self):

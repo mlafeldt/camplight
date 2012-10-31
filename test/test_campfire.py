3138b6abbe0327ac05f81d538a9cec15b69556c0
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-10-31 17:05:11 +0100

    Rewrite tests to use pytest

--- test/test_campfire.py
+++ test/test_campfire.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import unittest
+import pytest
 import mock
 import camplight
 
 
-class CampfireTest(unittest.TestCase):
+class TestCampfire(object):
 
-    def setUp(self):
+    def setup_method(self, method):
         self.request = mock.Mock()
         self.campfire = camplight.Campfire(self.request)
 
@@ -16,36 +16,37 @@ class CampfireTest(unittest.TestCase):
         self.request.get.return_value = {'account': expect}
         result = self.campfire.account()
         self.request.get.assert_called_once_with('/account')
-        self.assertEqual(result, expect)
+        assert result == expect
 
     def test_rooms(self):
         expect = [{'name': 'Serious'}, {'name': 'Danger'}]
         self.request.get.return_value = {'rooms': expect}
         result = self.campfire.rooms()
         self.request.get.assert_called_once_with('/rooms')
-        self.assertEqual(result, expect)
+        assert result == expect
 
     def test_room_by_name(self):
         rooms = [{'name': 'Serious', 'id': 1000}, {'name': 'Danger', 'id': 2000}]
         self.campfire.rooms = mock.Mock(return_value=rooms)
         result = self.campfire.room('Serious')
-        self.assertEqual(result.room_id, 1000)
+        assert result.room_id == 1000
 
     def test_room_by_id(self):
         result = self.campfire.room(3000)
-        self.assertEqual(result.room_id, 3000)
+        assert result.room_id == 3000
 
     def test_room_not_found(self):
         rooms = [{'name': 'Serious', 'id': 1000}]
         self.campfire.rooms = mock.Mock(return_value=rooms)
-        self.assertRaises(camplight.RoomNotFoundError, self.campfire.room, 'Danger')
+        with pytest.raises(camplight.RoomNotFoundError):
+            self.campfire.room('Danger')
 
     def test_user_me(self):
         expect = {'name': 'John Doe', 'email_address': 'john.doe@gmail.com'}
         self.request.get.return_value = {'user': expect}
         result = self.campfire.user()
         self.request.get.assert_called_once_with('/users/me')
-        self.assertEqual(result, expect)
+        assert result == expect
 
     def test_user_other(self):
         expect = {'name': 'Alan Turing'}
@@ -53,22 +54,22 @@ class CampfireTest(unittest.TestCase):
         user_id = 6789
         result = self.campfire.user(user_id)
         self.request.get.assert_called_once_with('/users/%s' % user_id)
-        self.assertEqual(result, expect)
+        assert result == expect
 
     def test_presence(self):
         expect = [{'name': 'Danger'}]
         self.request.get.return_value = {'rooms': expect}
         result = self.campfire.presence()
         self.request.get.assert_called_once_with('/presence')
-        self.assertEqual(result, expect)
+        assert result == expect
 
     def test_search(self):
         expect = [{'body': 'ohai', 'type': camplight.MessageType.TEXT}]
         self.request.get.return_value = {'messages': expect}
         result = self.campfire.search('ohai')
         self.request.get.assert_called_once_with('/search/ohai')
-        self.assertEqual(result, expect)
+        assert result == expect
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main(__file__)
--- test/test_room.py
+++ test/test_room.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import unittest
+import pytest
 import random
 import mock
 import camplight
 
 
-class RoomTest(unittest.TestCase):
+class TestRoom(object):
 
-    def setUp(self):
+    def setup_method(self, method):
         self.request = mock.Mock()
         self.room_id = random.randint(1, 1000000)
         self.room = camplight.Room(self.request, self.room_id)
@@ -18,48 +18,48 @@ class RoomTest(unittest.TestCase):
         self.request.get.return_value = {'room': expect}
         result = self.room.status()
         self.request.get.assert_called_once_with('/room/%s' % self.room_id)
-        self.assertEqual(result, expect)
+        assert result == expect
 
     def test_recent(self):
         expect = [{'body': 'Hello World', 'type': camplight.MessageType.TEXT}]
         self.request.get.return_value = {'messages': expect}
         result = self.room.recent()
         self.request.get.assert_called_once_with('/room/%s/recent' % self.room_id)
-        self.assertEqual(result, expect)
+        assert result == expect
 
     def test_transcript(self):
         expect = [{'body': 'Hello World', 'type': camplight.MessageType.TEXT}]
         self.request.get.return_value = {'messages': expect}
         result = self.room.transcript()
         self.request.get.assert_called_once_with('/room/%s/transcript' % self.room_id)
-        self.assertEqual(result, expect)
+        assert result == expect
 
     def test_uploads(self):
         expect = [{'name': 'file.png', 'content_type': 'image/png'}]
         self.request.get.return_value = {'uploads': expect}
         result = self.room.uploads()
         self.request.get.assert_called_once_with('/room/%s/uploads' % self.room_id)
-        self.assertEqual(result, expect)
+        assert result == expect
 
     def test_join(self):
         result = self.room.join()
         self.request.post.assert_called_once_with('/room/%s/join' % self.room_id)
-        self.assertEqual(result, None)
+        assert result == None
 
     def test_leave(self):
         result = self.room.leave()
         self.request.post.assert_called_once_with('/room/%s/leave' % self.room_id)
-        self.assertEqual(result, None)
+        assert result == None
 
     def test_lock(self):
         result = self.room.lock()
         self.request.post.assert_called_once_with('/room/%s/lock' % self.room_id)
-        self.assertEqual(result, None)
+        assert result == None
 
     def test_unlock(self):
         result = self.room.unlock()
         self.request.post.assert_called_once_with('/room/%s/unlock' % self.room_id)
-        self.assertEqual(result, None)
+        assert result == None
 
     def test_speak(self):
         expect = {'body': 'Hello World'}
@@ -67,7 +67,7 @@ class RoomTest(unittest.TestCase):
         result = self.room.speak('Hello World')
         self.request.post.assert_called_once_with('/room/%s/speak' % self.room_id,
                                                   data={'message': expect})
-        self.assertEqual(result, expect)
+        assert result == expect
 
     def test_paste(self):
         expect = {'body': 'Hello World', 'type': camplight.MessageType.PASTE}
@@ -75,7 +75,7 @@ class RoomTest(unittest.TestCase):
         result = self.room.paste('Hello World')
         self.request.post.assert_called_once_with('/room/%s/speak' % self.room_id,
                                                   data={'message': expect})
-        self.assertEqual(result, expect)
+        assert result == expect
 
     def test_play(self):
         expect = {'body': camplight.Sound.YEAH, 'type': camplight.MessageType.SOUND}
@@ -83,20 +83,20 @@ class RoomTest(unittest.TestCase):
         result = self.room.play(camplight.Sound.YEAH)
         self.request.post.assert_called_once_with('/room/%s/speak' % self.room_id,
                                                   data={'message': expect})
-        self.assertEqual(result, expect)
+        assert result == expect
 
     def test_set_name(self):
         result = self.room.set_name('Danger')
         self.request.put.assert_called_once_with('/room/%s' % self.room_id,
                                                  data={'room': {'name': 'Danger'}})
-        self.assertEqual(result, None)
+        assert result == None
 
     def test_set_topic(self):
         result = self.room.set_topic('No serious discussion')
         self.request.put.assert_called_once_with('/room/%s' % self.room_id,
                                                  data={'room': {'topic': 'No serious discussion'}})
-        self.assertEqual(result, None)
+        assert result == None
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main(__file__)

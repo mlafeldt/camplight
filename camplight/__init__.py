feab7da112049fb949e020e6a74c5cca606aafdc
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-07-10 12:59:16 +0200

    v0.9.3

--- camplight/__init__.py
+++ camplight/__init__.py
@@ -10,7 +10,7 @@ Python implementation of Campfire API
 
 __author__ = 'Mathias Lafeldt <mathias.lafeldt@gmail.com>'
 __license__ = 'MIT'
-__version__ = '0.9.2'
+__version__ = '0.9.3'
 
 from .api import *
 from .exceptions import *
